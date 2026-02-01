db5114a54bed90db8f4d4584dccde41a0adb265b
1004yeeun <joycejeoung@ucla.edu>
2026-02-01 06:12:46 -0800

    Parse excel file and fill agenda_items table

--- import_agenda.py
+++ import_agenda.py
@@ -1,15 +1,56 @@
+#!/usr/bin/env python3
+
 ### I. Import Agenda
 
-# This program imports the schedule of an event into a local SQLite database.
+## Input: agenda.xls
+## Output: 
+#   Imported xxx sessions, xxx subsessions, xxx speakers, xxx mappings into interview_test.db
+#   Error: file not found OR missing args/bad file OR parse fail OR ...
 
-# To complete this task, you will need to:
-# 1. Open an Agenda excel file
-# 2. Design SQLite Database table schema(s) to store agenda information
-# 3. Parse the content of the excel file and store the content in the table(s) you designed
+import sys, os, xlrd
+from db import open_tables, close_tables
+from db_table import db_table
 
-# We should be able to run your program as follows:\
-# `$> ./import_agenda.py agenda.xls`
+HEADER_ROW = 14 # (for now)
+DATA_START_ROW = 15 # (for now)
 
-# Please note:
-# * The input file will always follow the same format as the one provided in this repository.
-# * We do not expect any specific output.
+def main():
+    argv = sys.argv[1:]
+
+    # require exactly one arg (for now)
+    if len(argv) != 1:
+        print("Error: expected exactly one argument.")
+        return 2
+    
+    # check file exists
+    agenda_path = argv[0]
+    if not os.path.isfile(agenda_path):
+        print(f"Error: file '{agenda_path}' not found.")
+        return 2
+    
+    try:
+        agenda_items, speakers, agenda_item_to_speaker = open_tables()
+        book = xlrd.open_workbook(agenda_path)
+        sh = book.sheet_by_index(0)
+
+        for i in range(DATA_START_ROW, sh.nrows):
+            # fill agenda_items table
+            row = [str(x).replace("'", "''").strip() for x in sh.row_values(i)]
+            agenda_item_id = agenda_items.insert({
+                "row_index": i,
+                "date": row[0],
+                "time_start": row[1],
+                "time_end": row[2],
+                "session_type": row[3],
+                "parent_id": "",
+                "title": row[4],
+                "location": row[5],
+                "description": row[6]
+            })
+    
+    finally:
+        if agenda_items and speakers and agenda_item_to_speaker:
+            close_tables(agenda_items, speakers, agenda_item_to_speaker)
+
+if __name__ == "__main__":
+    main()
